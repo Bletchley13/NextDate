fc85cf57d084ebc9c957529dddef74e1a0d2c23d
bletchley <bletchley@Viking.(none)>
2014-10-20 22:06:33 +0800

    Add testing wrapper

--- NextDate.h
+++ NextDate.h
@@ -1,11 +1,15 @@
-
+#include <stdio.h>
 
 typedef struct _Date{
 	int year;
-	int mounth;
+	int month;
 	int day;
 } Date;
 
-Date *NextDate(Date);  
+Date *NextDate(Date*);  
+Date *NewDateInstance(int year, int month, int day);
+void ShowDate(Date*);
+bool checkDate(Date* a, int true_year, int true_mouth, int true_day);
+
 
 
